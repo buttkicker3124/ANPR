877688e9fc75cc09879a2a87e583ff0dda42df04
Adityaverma <adityaverma4648@gmail.com>
2022-07-02 19:09:23 +0530

    frontend begining

---
+++ ANPR_WebApp/frontend/my-app/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe at magnam ipsa?
+    </div>
+  );
+}
+
+export default App;
